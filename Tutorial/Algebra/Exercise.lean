3c69ef0f130098190d7279606b8350f332d055c8
Kaonan Micadei <6026964+kaosmicadei@users.noreply.github.com>
2026-09-04 10:30:25 +0200

    Add parity of n(n+1)

--- Tutorial/Algebra/Exercise.lean
+++ Tutorial/Algebra/Exercise.lean
@@ -11,3 +11,19 @@ example (a b : Nat) : (a + b) * (a + b) = a*a + b*b + 2*a*b := by
   simp
   rw [Nat.mul_comm b 2, ← Nat.mul_assoc, Nat.mul_comm a 2]
   rw [← Nat.add_assoc]
+
+def Even (n : Nat) : Prop := ∃ k : Nat, n = 2*k
+def Odd (n : Nat) : Prop := ∃ k : Nat, n = 2*k + 1
+
+theorem mul_consec_even (n : Nat) : Even (n * (n + 1)) := by
+  unfold Even
+  induction n with
+  | zero => exists 0
+  | succ m ih =>
+    obtain ⟨k, hk⟩ := ih
+    rw [Nat.add_assoc]
+    simp
+    rw [Nat.mul_add, Nat.mul_comm, hk]
+    rw [Nat.add_mul, Nat.mul_comm m, Nat.one_mul, ← Nat.add_assoc]
+    exists (k + m + 1)
+    rw [Nat.mul_add, Nat.mul_add]
